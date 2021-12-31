635cfd8d332eb99c1611fcbaf08794c92e1b114b
Peter Cotton <petercotton@Peters-Mac-mini-2.local>
2021-12-30 21:20:06 -0500

    tests

---
+++ tests/qs/test_qs_equal_weight.py
@@ -0,0 +1,2 @@
+
+stocks = ['FB','GOOG','APPL']
--- tests/qs/test_qs_hello_world.py
+++ tests/qs/test_qs_hello_world.py
@@ -5,7 +5,7 @@ def test_hello_quantstats():
     qs.extend_pandas()
     stock = qs.utils.download_returns('FB')
     qs.stats.sharpe(stock)
-    
+
 
 if __name__=='__main__':
     test_hello_quantstats()
