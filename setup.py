452d5a5512ddc94de61cf7aff7e641d54ba5c152
Peter Cotton <peter.cotton@microprediction.com>
2022-01-26 23:12:47 -0500

    rfactoring

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="precise",
-    version="0.2.0",
+    version="0.3.0",
     description="Online covariance and precision estimation",
     long_description=README,
     long_description_content_type="text/markdown",
