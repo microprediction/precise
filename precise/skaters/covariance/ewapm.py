bdec1dfa43dfd0b63722bdc36e3a2d6855edd8ea
OVVO-Financial <ovvo.financialsystems@gmail.com>
2022-01-27 16:34:37 -0500

    Update ewapm.py
    Add `r=.005` for both mean and 0 target to the SKATERS list, and set `n_emp=100` for `r>.01`

--- precise/skaters/covariance/ewapm.py
+++ precise/skaters/covariance/ewapm.py
@@ -10,25 +10,29 @@ def ewa_pm_emp_scov_r01(s, y, k=1):
 
 
 def ewa_pm_emp_scov_r02(s, y, k=1):
-    return partial_ema_scov_factory(s=s,y=y,k=k,r=0.02)
+    return partial_ema_scov_factory(s=s,y=y,k=k,r=0.02,n_emp=100)
 
 
 def ewa_pm_emp_scov_r05(s, y, k=1):
-    return partial_ema_scov_factory(s=s,y=y,k=k,r=0.05)
+    return partial_ema_scov_factory(s=s,y=y,k=k,r=0.05,n_emp=100)
+
+
+ """ target = 0 """
+def ewa_pm_emp_scov_r005_t0(s, y, k=1):
+    return partial_ema_scov_factory(s=s,y=y,k=k,r=0.005, target=0)
 
 
 def ewa_pm_emp_scov_r01_t0(s, y, k=1):
-    """ target = 0 """
     return partial_ema_scov_factory(s=s,y=y,k=k,r=0.01, target=0)
 
 
 def ewa_pm_emp_scov_r02_t0(s, y, k=1):
-    return partial_ema_scov_factory(s=s,y=y,k=k,r=0.02, target=0)
+    return partial_ema_scov_factory(s=s,y=y,k=k,r=0.02, target=0,n_emp=100)
 
 
 def ewa_pm_emp_scov_r05_t0(s, y, k=1):
-    return partial_ema_scov_factory(s=s,y=y,k=k,r=0.05, target=0)
+    return partial_ema_scov_factory(s=s,y=y,k=k,r=0.05, target=0,n_emp=100)
 
 
-EWA_PM_EMP_D0_COV_SKATERS = [ewa_pm_emp_scov_r01, ewa_pm_emp_scov_r02, ewa_pm_emp_scov_r05,
-                             ewa_pm_emp_scov_r01_t0, ewa_pm_emp_scov_r02_t0, ewa_pm_emp_scov_r05_t0]
+EWA_PM_EMP_D0_COV_SKATERS = [ewa_pm_emp_scov_r005, ewa_pm_emp_scov_r01, ewa_pm_emp_scov_r02, ewa_pm_emp_scov_r05,
+                             ewa_pm_emp_scov_r005_t0, ewa_pm_emp_scov_r01_t0, ewa_pm_emp_scov_r02_t0, ewa_pm_emp_scov_r05_t0]
