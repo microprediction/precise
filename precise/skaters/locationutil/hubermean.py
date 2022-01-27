c1a9eab7be15d114227e191c8b551673e6f4c3cb
Peter Cotton <peter.cotton@microprediction.com>
2022-01-27 12:10:40 -0500

    crud

--- precise/skaters/locationutil/hubermean.py
+++ precise/skaters/locationutil/hubermean.py
@@ -138,18 +138,3 @@ def mean_quadratic_error(mu, xs):
     y = xs - mu_rep
     return np.nanmean(y**2, axis=0)
 
-
-if __name__=='__main__':
-    n_vars = 5
-    n_samples = 500
-    xs = np.random.randn(n_samples,n_vars)
-    a = 1.0
-    b = 1.5
-    x_median = np.median(xs,axis=0)
-    x_mean = np.mean(xs,axis=0)
-    lb = np.where( x_median < x_mean, x_median, x_mean )
-    ub = np.where( x_median > x_mean, x_median, x_mean)
-
-    xStar, all_converged = parallel_bisection_root_finder(f=huber_deriv, lb=lb, ub=ub, a=a, b=b, xs=xs)
-    print(xStar)
-    print(all_converged)
