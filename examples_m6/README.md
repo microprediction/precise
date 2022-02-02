ec3b9d82a39f88858a9083b191d9478ee7f4f68c
Peter Cotton <peter.cotton@microprediction.com>
2022-02-02 13:37:45 -0500

    m6

---
+++ examples_m6/README.md
@@ -0,0 +1,4 @@
+
+
+Demonstrates one way to create an entry in the M6 Financial Forecasting competition 
+
--- examples_m6/example_m6_entry.py
+++ examples_m6/example_m6_entry.py
@@ -5,7 +5,7 @@ import time
 
 
 if __name__=='__main__':
-    n_dim = 100 # <-- 100 for real thing, less for testing
+    n_dim = 10 # <-- 100 for real thing, less for testing
     df = m6_competition_entry(n_dim=n_dim)
     classification = 'full' if (n_dim==100) else 'partial'
     timestamped_csv_file = os.path.join(M6_EXAMPLES,classification,'m6_'+time.strftime("%Y%m%d-%H%M%S")+'.csv')
