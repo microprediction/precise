e71c26e8e186009e332d2914b92dbf1224804c18
Peter Cotton <peter.cotton@microprediction.com>
2022-03-02 15:35:38 -0500

    elos for cov estimation

---
+++ examples_elo_ratings/view_cov_elo_ratings.py
@@ -0,0 +1,9 @@
+from precise.skatervaluation.battleutil.compilingeloratings import elo_from_win_files
+from pprint import pprint
+
+
+if __name__=='__main__':
+    category_sub_string = 'stocks_5_days'  # e.g. m6_daily, p100, whatever
+    ratings = elo_from_win_files(genre='cov_likelihood')
+    pprint([r for r in ratings if category_sub_string in r[0]])
+
