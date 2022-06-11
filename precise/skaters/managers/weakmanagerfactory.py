f70ae2b76b4f1fdd5f5b2bba8bbb0716152e95cf
Peter Cotton <peter.cotton@microprediction.com>
2022-06-10 20:15:25 -0400

    weak fix

--- precise/skaters/managers/weakmanagerfactory.py
+++ precise/skaters/managers/weakmanagerfactory.py
@@ -5,27 +5,27 @@ from precise.skaters.covariance.ewaempfactory import ewa_emp_pcov_factory
 from functools import partial
 
 
-def weak_pm_manager_factory(y, s, target, n_emp,r, e=1, a=1.0, b=None, h=BIG_H, zeta=0):
+def weak_pm_manager_factory(y, s, target, n_emp,r, e=1, a=1.0, b=None, h=BIG_H, zeta=0,j=1):
     """
        Weak portfolio construction using partial moments cov estimation
     """
     f = partial( ewa_pm_factory, k=1,r=r,target=target, n_emp=n_emp )
     port = partial( weak_portfolio_factory, a=a, b=b, h=h )
-    return static_cov_manager_factory_d0(f=f, port=port, y=y, e=e, s=s, zeta=zeta)
+    return static_cov_manager_factory_d0(f=f, port=port, y=y, e=e, s=s, zeta=zeta,j=j)
 
 
-def weak_ewa_manager_factory(y, s, n_emp, r , e=1, a=1.0, b=None, h=BIG_H, zeta=0):
+def weak_ewa_manager_factory(y, s, n_emp, r , e=1, a=1.0, b=None, h=BIG_H, zeta=0,j=1):
     """
        Weak portfolio construction using partial moments cov estimation
     """
     f = partial( ewa_emp_pcov_factory, k=1,r=r, n_emp=n_emp )
     port = partial( weak_portfolio_factory, a=a, b=b, h=h )
-    return static_cov_manager_factory_d0(f=f, port=port, y=y, e=e, s=s, zeta=zeta)
+    return static_cov_manager_factory_d0(f=f, port=port, y=y, e=e, s=s, zeta=zeta,j=j)
 
 
-def weak_manager_factory(y, s, f, a=1.0, e=1, b=None,  h=BIG_H, zeta=0):
+def weak_manager_factory(y, s, f, a=1.0, e=1, b=None,  h=BIG_H, zeta=0,j=1):
     """
        Weak portfolio construction using any f
     """
     port = partial( weak_portfolio_factory, a=a, b=b, h=h )
-    return static_cov_manager_factory_d0(f=f, port=port, y=y, s=s, e=e, zeta=zeta)
+    return static_cov_manager_factory_d0(f=f, port=port, y=y, s=s, e=e, zeta=zeta,j=j)
--- precise/skaters/managers/weakmanagers.py
+++ precise/skaters/managers/weakmanagers.py
@@ -5,89 +5,89 @@ from precise.skaters.managers.buyandholdfactory import buy_and_hold
 
 USE_JS = False
 
-def weak_pm_t0_d0_r025_n50_long_manager(y, s, k=1, e=1):
+def weak_pm_t0_d0_r025_n50_long_manager(y, s, k=1, e=1,j=1):
     assert k==1
-    return weak_pm_manager_factory(y=y,s=s, target=0, r=0.025, n_emp=50, e=e)
+    return weak_pm_manager_factory(y=y,s=s, target=0, r=0.025, n_emp=50, e=e,j=j)
 
 
-def weak_ewa_t0_d0_r025_n50_long_manager(y, s, k=1, e=1):
+def weak_ewa_t0_d0_r025_n50_long_manager(y, s, k=1, e=1,j=1):
     assert k==1
-    return weak_ewa_manager_factory(y=y,s=s, r=0.025, n_emp=50, e=e)
+    return weak_ewa_manager_factory(y=y,s=s, r=0.025, n_emp=50, e=e,j=j)
 
 
-def weak_pm_t0_d0_r050_n50_long_manager(y, s, k=1, e=1):
+def weak_pm_t0_d0_r050_n50_long_manager(y, s, k=1, e=1,j=1):
     assert k==1
-    return weak_pm_manager_factory(y=y,s=s, target=0, r=0.050, n_emp=50, e=e)
+    return weak_pm_manager_factory(y=y,s=s, target=0, r=0.050, n_emp=50, e=e,j=j)
 
 
-def weak_ewa_t0_d0_r050_n50_long_manager(y, s, k=1, e=1):
+def weak_ewa_t0_d0_r050_n50_long_manager(y, s, k=1, e=1,j=1):
     assert k==1
-    return weak_ewa_manager_factory(y=y,s=s, r=0.050, n_emp=50, e=e)
+    return weak_ewa_manager_factory(y=y,s=s, r=0.050, n_emp=50, e=e,j=j)
 
 
 # Weak with strong entropish constraint
 
 
-def weak_pm_t0_d0_r025_n50_h125_long_manager(y, s, k=1, e=1):
+def weak_pm_t0_d0_r025_n50_h125_long_manager(y, s, k=1, e=1,j=1):
     assert k==1
-    return weak_pm_manager_factory(y=y,s=s, target=0, r=0.025, n_emp=50, h=1.25, e=e)
+    return weak_pm_manager_factory(y=y,s=s, target=0, r=0.025, n_emp=50, h=1.25, e=e,j=j)
 
 
-def weak_ewa_t0_d0_r025_n50_h125_long_manager(y, s, k=1, e=1):
+def weak_ewa_t0_d0_r025_n50_h125_long_manager(y, s, k=1, e=1,j=1):
     assert k==1
-    return weak_ewa_manager_factory(y=y,s=s, r=0.025, n_emp=50, h=1.25, e=e)
+    return weak_ewa_manager_factory(y=y,s=s, r=0.025, n_emp=50, h=1.25, e=e,j=j)
 
 
-def weak_pm_t0_d0_r050_n50_h125_long_manager(y, s, k=1, e=1):
+def weak_pm_t0_d0_r050_n50_h125_long_manager(y, s, k=1, e=1,j=1):
     assert k==1
-    return weak_pm_manager_factory(y=y,s=s, target=0, r=0.050, n_emp=50, h=1.25, e=e)
+    return weak_pm_manager_factory(y=y,s=s, target=0, r=0.050, n_emp=50, h=1.25, e=e,j=j)
 
 
-def weak_ewa_t0_d0_r050_n50_h125_long_manager(y, s, k=1, e=1):
+def weak_ewa_t0_d0_r050_n50_h125_long_manager(y, s, k=1, e=1,j=1):
     assert k==1
-    return weak_ewa_manager_factory(y=y,s=s, r=0.050, n_emp=50, h=1.25, e=e)
+    return weak_ewa_manager_factory(y=y,s=s, r=0.050, n_emp=50, h=1.25, e=e,j=j)
 
 # Weak with mild entropish constraint
 
 
-def weak_pm_t0_d0_r025_n50_h500_long_manager(y, s, k=1, e=1):
+def weak_pm_t0_d0_r025_n50_h500_long_manager(y, s, k=1, e=1,j=1):
     assert k==1
-    return weak_pm_manager_factory(y=y,s=s, target=0, r=0.025, n_emp=50, h=5, e=e)
+    return weak_pm_manager_factory(y=y,s=s, target=0, r=0.025, n_emp=50, h=5, e=e,j=j)
 
 
-def weak_ewa_t0_d0_r025_n50_h500_long_manager(y, s, k=1, e=1):
+def weak_ewa_t0_d0_r025_n50_h500_long_manager(y, s, k=1, e=1,j=1):
     assert k==1
-    return weak_ewa_manager_factory(y=y,s=s, r=0.025, n_emp=50, h=5, e=e)
+    return weak_ewa_manager_factory(y=y,s=s, r=0.025, n_emp=50, h=5, e=e,j=j)
 
 
-def weak_pm_t0_d0_r050_n50_h500_long_manager(y, s, k=1, e=1):
+def weak_pm_t0_d0_r050_n50_h500_long_manager(y, s, k=1, e=1,j=1):
     assert k==1
-    return weak_pm_manager_factory(y=y,s=s, target=0, r=0.050, n_emp=50, h=5, e=e)
+    return weak_pm_manager_factory(y=y,s=s, target=0, r=0.050, n_emp=50, h=5, e=e,j=j)
 
 
-def weak_ewa_t0_d0_r050_n50_h500_long_manager(y, s, k=1, e=1):
+def weak_ewa_t0_d0_r050_n50_h500_long_manager(y, s, k=1, e=1,j=1):
     assert k==1
-    return weak_ewa_manager_factory(y=y,s=s, r=0.050, n_emp=50, h=5, e=e)
+    return weak_ewa_manager_factory(y=y,s=s, r=0.050, n_emp=50, h=5, e=e,j=j)
 
 
 
 # Sklean
 
 
-def weak_sk_lw_pcov_d0_n100_long_manager(y,s,k=1, e=1):
-    return weak_manager_factory(y=y,s=s,f=buf_sk_lw_pcov_d0_n100, e=e)
+def weak_sk_lw_pcov_d0_n100_long_manager(y,s,k=1, e=1,j=1):
+    return weak_manager_factory(y=y,s=s,f=buf_sk_lw_pcov_d0_n100, e=e,j=j)
 
 
-def weak_sk_glcv_pcov_d0_n100_long_manager(y,s,k=1, e=1):
-    return weak_manager_factory(y=y,s=s,f=buf_sk_glcv_pcov_d0_n100, e=e)
+def weak_sk_glcv_pcov_d0_n100_long_manager(y,s,k=1, e=1,j=1):
+    return weak_manager_factory(y=y,s=s,f=buf_sk_glcv_pcov_d0_n100, e=e,j=j)
 
 
-def weak_sk_glcv_pcov_d0_n100_t0_long_manager(y,s,k=1, e=1):
-    return weak_manager_factory(y=y,s=s,f=buf_sk_glcv_pcov_d0_n100_t0, e=e)
+def weak_sk_glcv_pcov_d0_n100_t0_long_manager(y,s,k=1, e=1,j=1):
+    return weak_manager_factory(y=y,s=s,f=buf_sk_glcv_pcov_d0_n100_t0, e=e,j=j)
 
 
-def weak_sk_mcd_pcov_d0_n100_long_manager(y,s,k=1, e=1):
-    return weak_manager_factory(y=y,s=s,f=buf_sk_mcd_pcov_d0_n100, e=e)
+def weak_sk_mcd_pcov_d0_n100_long_manager(y,s,k=1, e=1,j=1):
+    return weak_manager_factory(y=y,s=s,f=buf_sk_mcd_pcov_d0_n100, e=e,j=j)
 
 
 WEAK_J1_LONG_MANAGERS = [weak_pm_t0_d0_r025_n50_long_manager,
