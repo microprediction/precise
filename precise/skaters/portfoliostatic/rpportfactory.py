bf0c8165adb5072e6a1d33ff7a9b3cf3b2ed9c44
Peter Cotton <peter.cotton@microprediction.com>
2022-07-14 07:03:38 -0400

    defense

--- precise/skaters/portfoliostatic/rpportfactory.py
+++ precise/skaters/portfoliostatic/rpportfactory.py
@@ -5,6 +5,7 @@ import riskparityportfolio as rp
 from precise.skaters.portfolioutil.portfunctions import var_scaled_returns
 import pandas as pd
 from precise.skaters.covarianceutil.covfunctions import multiply_off_diag
+from precise.skaters.portfoliostatic.equalport import equal_long_port
 
 # Thin wrapper for riskparityportfolio package
 
@@ -17,6 +18,8 @@ def rp_portfolio_factory(pre=None, cov=None, mu=0.02, risk_free_rate=0.02, phi=1
     cov = multiply_off_diag(cov,phi=phi)
     expected_returns = var_scaled_returns(cov=cov,mu=mu,r=risk_free_rate)
     w = rp.vanilla.design(cov, b=expected_returns)
+    if any([np.isnan(wi) for wi in w]):
+        return equal_long_port(cov=cov)
     return w
 
 
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="precise",
-    version="0.9.1",
+    version="0.9.2",
     description="Online covariance, precision, portfolios and ensembles",
     long_description=README,
     long_description_content_type="text/markdown",
