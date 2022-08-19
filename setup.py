59a4408fa55542789a9c9a8f68854a2f3cd5a75e
Peter Cotton <peter.cotton@microprediction.com>
2022-08-19 13:32:00 -0400

    data

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="precise",
-    version="0.10.26",
+    version="0.10.27",
     description="Online covariance, precision, portfolios and ensembles",
     long_description=README,
     long_description_content_type="text/markdown",
