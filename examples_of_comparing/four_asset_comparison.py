252e55911c23d4fb3f4de0ec247bfb88ac68e33b
Peter Cotton <peter.cotton@microprediction.com>
2022-08-19 14:30:31 -0400

    manangers

--- examples_of_comparing/four_asset_comparison.py
+++ examples_of_comparing/four_asset_comparison.py
@@ -1,15 +1,7 @@
-from precise.skaters.covarianceutil.covfunctions import cov_to_corrcoef, weaken_cov
-from precise.skaters.locationutil.vectorfunctions import normalize
-from precise.skaters.portfoliostatic.weakport import weak_long_port
-from precise.skaters.portfoliostatic.unitport import unit_port
-from precise.skaters.portfoliostatic.weakportfactory import _weak_from_cov
-from precise.skaters.portfolioutil.portfunctions import portfolio_variance
-import numpy as np
-from pprint import pprint
-from momentum.functions import var_init, var_update, kurtosis_init, kurtosis_update
 from pprint import pprint
 from precise.skaters.portfolioutil.portcomparison import port_kurtosis
 from precise.skaters.portfoliostatic.allstaticport import LONG_PORT
+import numpy as np
 
 # Comparing out-of-sample portfolio variance
 
