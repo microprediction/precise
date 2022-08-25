8ef194c01f4f060f99eb180b74f328db4e18d046
Peter Cotton <peter.cotton@microprediction.com>
2022-08-25 08:22:58 -0400

    elo

--- examples_elo_ratings/view_manager_elo_ratings.py
+++ examples_elo_ratings/view_manager_elo_ratings.py
@@ -3,7 +3,7 @@ from pprint import pprint
 
 
 if __name__=='__main__':
-    category_sub_string = 'stocks_1_days'  # e.g. m6_daily, p100, whatever
+    category_sub_string = 'stocks'  # e.g. m6_daily, p100, whatever
     ratings = elo_from_win_files(genre='manager_var')
     pprint([r for r in ratings if category_sub_string in r[0]])
 
