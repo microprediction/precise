b0a2a2e3adee1a0f4e65c1e4f2f8c31cc00dc404
Peter Cotton <peter.cotton@microprediction.com>
2022-08-25 12:46:37 -0400

    skater list

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="precise",
-    version="0.10.28",
+    version="0.10.29",
     description="Online covariance, precision, portfolios and ensembles",
     long_description=README,
     long_description_content_type="text/markdown",
