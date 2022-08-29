0e39e4a94f229d7b433bbe7ac566733635673ed5
Peter Cotton <peter.cotton@microprediction.com>
2022-08-29 11:12:20 -0400

    resutls

--- tests/battleutil/test_interpreting_elos.py
+++ tests/battleutil/test_interpreting_elos.py
@@ -289,4 +289,4 @@ if __name__=='__main__':
     elos = EXAMPLE_MANAGER_ELOS
     df = manager_regressor_frame(elos)
     from precise.whereami import BATTLE_RESULTS_DIR
-    df.to_csv(BATTLE_RESULTS_DIR+'/example_manager_regressor_frame.csv')
+    df.to_csv(BATTLE_RESULTS_DIR+'/example_manager_regressor_frame.csv', index=False)
