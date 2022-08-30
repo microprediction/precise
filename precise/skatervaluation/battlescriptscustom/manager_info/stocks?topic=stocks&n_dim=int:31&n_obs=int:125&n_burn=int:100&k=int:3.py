54f13bbe568d3bc08fe51923d3a8e0dd12dee737
Peter Cotton <peter.cotton@microprediction.com>
2022-08-29 20:13:25 -0400

    elo examples

---
+++ precise/skatervaluation/battlescriptscustom/manager_info/stocks?topic=stocks&n_dim=int:31&n_obs=int:125&n_burn=int:100&k=int:3.py
@@ -0,0 +1,13 @@
+import runthis
+from runthis import parse_kwargs
+import os
+from precise.skaters.managerutil.managerstats import manager_info, manager_var
+
+# Infers params from file name and runs a battle
+from precise.skaters.managers.schurmanagers import SCHUR_PM_S5_LONG_MANAGERS
+from precise.skaters.managers.hrpmanagers import HRP_LONG_MANAGERS
+from precise.skatervaluation.battleutil.arrangingbattles import generic_battle
+
+if __name__=='__main__':
+    params = parse_kwargs(__file__.split(os.path.sep)[-1])
+    generic_battle(contestants=SCHUR_PM_S5_LONG_MANAGERS+HRP_LONG_MANAGERS, evaluator=manager_info, params=params, atol=1e-8)
