faef02444ef9e70483c005b6b1cb7ca05770eccf
Peter Cotton <57455669+microprediction@users.noreply.github.com>
2022-12-24 08:11:16 -0500

    Update ldpmanagers.py

--- precise/skaters/managers/ldpmanagers.py
+++ precise/skaters/managers/ldpmanagers.py
@@ -6,33 +6,58 @@ from precise.skaters.managers.schurmanagerfactory import schur_diag_diag_buf_emp
 # (Note that seriation may be different so this is not supposed to be a replica)
 
 
+def ldp_s2_n50_long_manager(y, s, k=1, e=1, j=1, q=1.0):
+    assert k == 1
+    return schur_diag_diag_buf_emp_manager_factory(y=y, s=s, n_buffer=50, e=e, gamma=0, delta=0, j=j, q=q, n_split=2)
+
+
 def ldp_s5_n50_long_manager(y, s, k=1, e=1, j=1, q=1.0):
     assert k == 1
-    return schur_diag_diag_buf_emp_manager_factory(y=y, s=s, n_buffer=50, e=e, gamma=0, delta=0, j=j, q=q)
+    return schur_diag_diag_buf_emp_manager_factory(y=y, s=s, n_buffer=50, e=e, gamma=0, delta=0, j=j, q=q, n_split=5)
 
 
 def ldp_s25_n50_long_manager(y, s, k=1, e=1, j=1, q=1.0):
     assert k == 1
-    return schur_diag_diag_buf_emp_manager_factory(y=y, s=s, n_buffer=50, e=e, gamma=0, delta=0, j=j, q=q)
+    return schur_diag_diag_buf_emp_manager_factory(y=y, s=s, n_buffer=50, e=e, gamma=0, delta=0, j=j, q=q, n_split=25)
 
 
 def ldp_s5_n100_long_manager(y, s, k=1, e=1, j=1, q=1.0):
     assert k == 1
-    return schur_diag_diag_buf_emp_manager_factory(y=y, s=s, n_buffer=100, e=e, gamma=0, delta=0, j=j, q=q)
+    return schur_diag_diag_buf_emp_manager_factory(y=y, s=s, n_buffer=100, e=e, gamma=0, delta=0, j=j, q=q, n_split=5)
 
 
 def ldp_s25_n100_long_manager(y, s, k=1, e=1, j=1, q=1.0):
     assert k == 1
-    return schur_diag_diag_buf_emp_manager_factory(y=y, s=s, n_buffer=100, e=e, gamma=0, delta=0, j=j, q=q)
+    return schur_diag_diag_buf_emp_manager_factory(y=y, s=s, n_buffer=100, e=e, gamma=0, delta=0, j=j, q=q, n_split=25)
+
+
+# Conv hull managers 
+
+def ldp_s2_n200_l5_long_manager(y, s, k=1, e=1, j=1, q=1.0):
+    assert k == 1
+    return schur_diag_diag_buf_emp_manager_factory(y=y, s=s, n_buffer=100, e=e, gamma=0, delta=0, j=j, q=q, l=5, n_split=2)
+
+
+def ldp_s5_n200_l5_long_manager(y, s, k=1, e=1, j=1, q=1.0):
+    assert k == 1
+    return schur_diag_diag_buf_emp_manager_factory(y=y, s=s, n_buffer=100, e=e, gamma=0, delta=0, j=j, q=q, l=5, n_split=5)
+
+
+def ldp_s5_n200_l21_long_manager(y, s, k=1, e=1, j=1, q=1.0):
+    assert k == 1
+    return schur_diag_diag_buf_emp_manager_factory(y=y, s=s, n_buffer=100, e=e, gamma=0, delta=0, j=j, q=q, l=21, n_split=5)
 
 
 def ldp_s100_n200_l21_long_manager(y, s, k=1, e=1, j=1, q=1.0):
     assert k == 1
-    return schur_diag_diag_buf_emp_manager_factory(y=y, s=s, n_buffer=100, e=e, gamma=0, delta=0, j=j, q=q, l=21)
+    return schur_diag_diag_buf_emp_manager_factory(y=y, s=s, n_buffer=100, e=e, gamma=0, delta=0, j=j, q=q, l=21, n_split=100)
 
 
-LDP_LONG_MANAGERS = [ldp_s5_n50_long_manager, ldp_s25_n50_long_manager,
+
+
+LDP_LONG_MANAGERS = [ldp_s2_n50_long_manager, ldp_s5_n50_long_manager, ldp_s25_n50_long_manager,
                      ldp_s5_n100_long_manager, ldp_s25_n100_long_manager
-                     ]
+                     ldp_s5_n200_l5_long_manager, ldp_s5_n200_l21_long_manager,
+                     ldp_s100_n200_l21_long_manager, ldp_s2_n200_l5_long_manager]
 HRP_LS_NAMED_MANAGERS = []
 HRP_NAMED_MANAGERS = LDP_LONG_MANAGERS + HRP_LS_NAMED_MANAGERS
