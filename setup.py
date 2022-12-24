e5d1f95ba69d166b6a7c6af77beeb61c881e05d0
Peter Cotton <57455669+microprediction@users.noreply.github.com>
2022-12-24 08:26:44 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="precise",
-    version="0.11.12",
+    version="0.11.13",
     description="Online covariance, precision, portfolios and ensembles",
     long_description=README,
     long_description_content_type="text/markdown",
