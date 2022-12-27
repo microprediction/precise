a7ad658e4b0ff4f172670373a62dcf7701e2d37c
Peter Cotton <57455669+microprediction@users.noreply.github.com>
2022-12-27 15:04:39 -0600

    Update diagallocfactory.py

--- precise/skaters/portfoliostatic/diagallocfactory.py
+++ precise/skaters/portfoliostatic/diagallocfactory.py
@@ -5,16 +5,16 @@ from itertools import zip_longest
 from precise.skaters.locationutil.vectorfunctions import normalize
 
 
-def diagonal_portfolio_variance(cov=None, pre=None):
+def diagonal_portfolio_variance(cov=None, pre=None, ridge=0.1):
     """
         Variance of the unit min-var portfolio
         (Used in some hierarchical methods to allocate capital)
     """
-    w = diagonal_portfolio_factory(pre=pre, cov=cov)
+    w = diagonal_portfolio_factory(pre=pre, cov=cov, ridge=ridge)
     return portfolio_variance(cov=cov,w=w)
 
 
-def diagonal_allocation_factory(covs:List, pres:List=None)->[float]:
+def diagonal_allocation_factory(covs:List, pres:List=None, ridge=0.1)->[float]:
     """ Allocate capital between portfolios using either cov or pre matrices
     :param covs:  List of covariance matrices
     :param pres:  List of precision matrices
@@ -23,6 +23,6 @@ def diagonal_allocation_factory(covs:List, pres:List=None)->[float]:
     if pres is None:
         pres = []
     # Remark: This was used in Marco Lopez de Prado's original HRP portfolio paper https://papers.ssrn.com/sol3/papers.cfm?abstract_id=2708678
-    return normalize([ 1/diagonal_portfolio_variance(cov=cov, pre=pre) for cov, pre in zip_longest(covs, pres, fillvalue=None) ])
+    return normalize([ 1/diagonal_portfolio_variance(cov=cov, pre=pre, ridge=ridge) for cov, pre in zip_longest(covs, pres, fillvalue=None) ])
 
 
