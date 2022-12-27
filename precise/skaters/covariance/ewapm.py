5fc543a4bc67e5c9febacc5702ff98caaaf92d7b
Peter Cotton <57455669+microprediction@users.noreply.github.com>
2022-12-27 11:15:13 -0600

    Update ewapm.py

--- precise/skaters/covariance/ewapm.py
+++ precise/skaters/covariance/ewapm.py
@@ -29,12 +29,20 @@ def ewa_pm_emp_scov_r01_n100_t0(s, y, k=1, e=1):
     return ewa_pm_factory(s=s, y=y, k=k, r=0.01, n_emp=100, target=0)
 
 
+def ewa_pm_emp_scov_r01_n300(s, y, k=1, e=1):
+    return ewa_pm_factory(s=s, y=y, k=k, r=0.01, n_emp=300)
+
+
+def ewa_pm_emp_scov_r01_n300_t0(s, y, k=1, e=1):
+    return ewa_pm_factory(s=s, y=y, k=k, r=0.01, n_emp=300, target=0)
+
+
 def ewa_pm_emp_scov_r01_n200(s, y, k=1, e=1):
-    return ewa_pm_factory(s=s, y=y, k=k, r=0.01, n_emp=100)
+    return ewa_pm_factory(s=s, y=y, k=k, r=0.01, n_emp=300)
 
 
 def ewa_pm_emp_scov_r01_n200_t0(s, y, k=1, e=1):
-    return ewa_pm_factory(s=s, y=y, k=k, r=0.01, n_emp=100, target=0)
+    return ewa_pm_factory(s=s, y=y, k=k, r=0.01, n_emp=200, target=0)
 
 
 def ewa_pm_emp_scov_r02_n50(s, y, k=1, e=1):
@@ -70,12 +78,12 @@ def ewa_pm_emp_scov_r05_n25_t0(s, y, k=1, e=1):
 
 
 
-EWA_PM_EMP_D0_COV_SKATERS_TM = [ewa_pm_emp_scov_r01_n100, ewa_pm_emp_scov_r01_n200,
+EWA_PM_EMP_D0_COV_SKATERS_TM = [ewa_pm_emp_scov_r01_n100, ewa_pm_emp_scov_r01_n200, ewa_pm_emp_scov_r01_n300,
                              ewa_pm_emp_scov_r02_n100, ewa_pm_emp_scov_r01_n200,
                              ewa_pm_emp_scov_r02_n50, ewa_pm_emp_scov_r02_n100,
                              ewa_pm_emp_scov_r05_n50, ewa_pm_emp_scov_r05_n25 ]
 
-EWA_PM_EMP_D0_COV_SKATERS_T0 = [ewa_pm_emp_scov_r01_n100_t0, ewa_pm_emp_scov_r01_n200_t0,
+EWA_PM_EMP_D0_COV_SKATERS_T0 = [ewa_pm_emp_scov_r01_n100_t0, ewa_pm_emp_scov_r01_n200_t0, ewa_pm_emp_scov_r01_n300_t0,
                              ewa_pm_emp_scov_r02_n100_t0, ewa_pm_emp_scov_r01_n200_t0,
                              ewa_pm_emp_scov_r02_n50_t0, ewa_pm_emp_scov_r02_n100_t0,
                              ewa_pm_emp_scov_r05_n50_t0, ewa_pm_emp_scov_r05_n25_t0 ]
