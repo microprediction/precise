f0f45d8cc31b1e1fc3add5b5d9b3d77a125fc7b9
Peter Cotton <57455669+microprediction@users.noreply.github.com>
2022-12-27 11:08:27 -0600

    Update weaksk.py

--- precise/skaters/covariance/weaksk.py
+++ precise/skaters/covariance/weaksk.py
@@ -1,10 +1,15 @@
 from precise.skaters.covariance.weakfactory import weak_pcov_factory
 from precise.skaters.covariance.bufsk import buf_sk_glcv_pcov_d0_n100_t0, buf_sk_lw_pcov_d0_n100, buf_sk_oas_pcov_d0_n100, buf_sk_mcd_pcov_d0_n100
-# Using structure learning
+from precise.skaters.covariance.bufsk import buf_sk_lw_pcov_d0_n200
+
 
 
 def weak_sk_oas_pcov_d0_n100(s, y, k=1, e=1):
     return weak_pcov_factory(f=buf_sk_glcv_pcov_d0_n100_t0, s=s, y=y, k=k, e=e)
+
+
+def weak_sk_lw_pcov_d0_n200(s, y, k=1, e=1):
+    return weak_pcov_factory(f=buf_sk_lw_pcov_d0_n200, s=s, y=y, k=k, e=e)
 
 
 def weak_sk_lw_pcov_d0_n100(s, y, k=1, e=1):
@@ -21,4 +26,5 @@ def weak_sk_mcd_pcov_d0_n100(s, y, k=1, e=1):
 
 
 WEAK_SK_DO_COV_SKATERS = [weak_sk_oas_pcov_d0_n100, weak_sk_lw_pcov_d0_n100,
-                          weak_sk_oas_pcov_d0_n100, weak_sk_mcd_pcov_d0_n100]
+                          weak_sk_oas_pcov_d0_n100, weak_sk_mcd_pcov_d0_n100,
+                         weak_sk_lw_pcov_d0_n200]
